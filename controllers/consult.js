845dec314238974ff95089ed9a496b72a8af6231
Derriel Collins <derrielcollins96@gmail.com>
2022-04-11 16:18:35 -0500

    text message functionality working with front-end correctly

---
+++ controllers/consult.js
@@ -0,0 +1,22 @@
+const handleConsult = (req, res,accountSid, authToken) => {
+	const { data } = req.body;
+	let tempBody = "";
+	for (const [key, value] of Object.entries(data)) {
+		tempBody = tempBody+` ${key}:${value}\n`;
+	}
+	console.log(tempBody)
+	if (!data) {
+		return res.status(400).json('incorrect form submission');
+	}
+	const client = require('twilio')(accountSid, authToken);
+     client.messages.create({
+	to: '+14698918404',
+	from: '+16823280293',
+	body: tempBody,
+})
+.then(message => console.log('Message sent successfully')); 
+}
+
+module.exports = {
+	handleConsult:handleConsult
+};
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const cors = require('cors')
-
+const cors = require('cors');
 const app = express();
+const consult = require('./controllers/consult.js');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const database = { };
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+// const client = require('twilio')(accountSid, authToken);
+// client.messages.create({
+// 	to: '+14698918404',
+// 	from: '+16823280293',
+// 	body: 'This will come from the front end Input at some point',
+// })
+// .then(message => console.log(message.sid)); 
 
 app.get('/', (req, res)=> {
 	console.log('/home')
 
-
 });
+app.post('/consult', (req, res) => {consult.handleConsult(req, res, accountSid, authToken)});
 
 
 
